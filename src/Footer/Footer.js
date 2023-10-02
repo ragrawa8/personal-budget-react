4b2be52d56048ef65261877690d434ece693aeec
Rhythm <ragrawa8@uncc.edu>
2023-10-01 21:29:52 -0400

    Brought P-Budget code In

---
+++ src/Footer/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer className="bottom">
+      <div className="center">
+        <p itemProp="author">All rights reserved &copy; Fabio Nolasco</p>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
